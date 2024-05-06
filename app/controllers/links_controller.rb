3d1a496e7af82a8118f36d23ce970ebdf72bcd10
OmarMWarraich <earthling986@gmail.com>
2024-05-06 21:20:19 +0500

    style: Enhance Design

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,8 @@
 class LinksController < ApplicationController
-  before_action :set_link,  only: [:show]
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
   def index
     @links = Link.recent_first
+    @link ||= Link.new
   end
 
   def show
@@ -11,11 +12,28 @@ class LinksController < ApplicationController
   def create
     @link = Link.new(link_params)
     if @link.save
-      redirect_to links_path
+      redirect_to root_path
     else
       index
       render :index, status: :unprocessable_entity
     end
+  end
+
+  def edit
+
+  end
+
+   def update
+    if @link.update(link_params)
+      redirect_to @link
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to root_path, notice: "Link has been deleted."
   end
 
   private
