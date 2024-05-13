7df6e6eeb540aca199a1637ae209a6433fd4242f
OmarMWarraich <earthling986@gmail.com>
2024-05-13 18:39:18 +0500

    feat: Pagination w/ Pagy

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+100.times do |i|
+  Link.create(url: "www.example.org")
+end
